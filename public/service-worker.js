d519e9f36dd004632545c7f00b6f3cea3a6fc7e7
IsaacAkanbi <isaac.akanbi@hotmail.co.uk>
2021-07-12 21:07:41 -0400

    changed file paths

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,11 +5,11 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/favicon.ico",
-  "/manifest.json",
-  "/public/style.css",
-  "/public/icons/icon-192x192.png",
-  "/public/icons/icon-512x512.png",
-  "/public/db.js", 
+  "/manifest.webmanifest",
+  "/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/db.js", 
  
 ];
 
